c11c052acf35f2dd74896da8ef6c15bf42755ffe
배광민 <baegwangmin@baegwangmin-ui-MacBook-Pro.local>
2019-05-22 13:49:11 +0900

    cafe24에서 war 배포를 위한 설정 변경및 실서버용 소스 수정

--- src/main/java/com/billlog/salsaexample/SalsaexampleApplication.java
+++ src/main/java/com/billlog/salsaexample/SalsaexampleApplication.java
@@ -2,12 +2,18 @@ package com.billlog.salsaexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SalsaexampleApplication {
+public class SalsaexampleApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SalsaexampleApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SalsaexampleApplication.class);
+    }
 }
